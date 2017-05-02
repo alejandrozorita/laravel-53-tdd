7c0a835560a2a6d41fc58a4f47aeefe19bc7f4bd
Alejandro Zorita Hernández <alejandro.zorita@consultaclick.es>
2017-05-02 17:42:27 +0200

    Cambios BD Test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,8 +1,20 @@
 <?php
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+
+    // Cargamos todas las BD en cada ejecución dejando la BD en blanco
+    // use DatabaseMigrations;
+    //
+    // Usaremos este comando para evitar que teniendo
+    // muchas migraciones, las pruebas se hagan lentas,
+    // por lo que se ejecutarán en transanciones
+    use DatabaseTransactions;
+
     /**
      * A basic functional test example.
      *
@@ -10,12 +22,13 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
+
         $name = 'Alejandro Zorita';
         $email = 'info@alejandrozorita.me';
 
         $user = factory(\App\User::class)->create([
-            'name'  => $name,
-            'email' => $email,
+            'name' => $name,
+            'email' => $email
         ]);
 
         $this->actingAs($user, 'api');
